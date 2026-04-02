1465a665c9688236c9ccaf1e9859aed8d4bbd780
bits0002 <emmanuel.bitsindou@etudiant.univ-reims.fr>
2026-04-02 09:35:04 +0200

    Ajout de fichier de configuration

---
+++ src/arduino-NextPM/NextPM_config.h
@@ -0,0 +1,16 @@
+
+
+#define Serial_LN(X)                                Serial.println(X)
+#define Serial_L(X)                                 Serial.print(X)
+
+
+
+#define Adress                                      1
+
+#define NEXTPM_TIME_DEFAULT                         1000
+#define NEXTPM_TIME_WARM_UP                         10000
+
+#define NEXTPM_BAUD                                 115200
+#define NEXTPM_PARITY                               SERIAL_8E1
+
+
